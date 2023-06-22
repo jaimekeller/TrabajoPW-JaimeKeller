1967fad5d2c179b42d9500102b1eda74792d1793
jaimekeller <jaimekellersa@gmail.com>
2023-06-21 22:20:54 -0300

    cambios

--- myScript.js
+++ myScript.js
@@ -1,15 +1,18 @@
+
 function realizarPedido() {
-    var nombre = document.getElementById("nombre").value;
+    var nombre = document.getElementById("nombre_pedido").value;
     var direccion = document.getElementById("direccion").value;
     var cantidad = document.getElementById("cantidad").value;
     var restaurante = document.getElementById('restaurante').value;
-    
-    if (nombre && direccion && cantidad > 0 && restaurante) {
-      alert("Pedido realizado de " + restaurante + '\n' + "\nNombre: " + nombre + "\nDirección: " + direccion + "\nCantidad: " + cantidad);
+    var hamburguesa = document.getElementById('hamburguesa').value;
+
+    if (nombre && direccion && cantidad > 0 && restaurante && hamburguesa) {
+      alert("Pedido realizado de " + restaurante + "\n" + "\nNombre: " + nombre + "\nDirección: " + direccion + "\nCantidad: " + cantidad);
+      
     } else {
       alert("Por favor complete todos los campos.");
     }
-}
+  }
 
 var pedidoBtn = document.getElementById("pedido-btn");
 pedidoBtn.addEventListener("click", realizarPedido);
